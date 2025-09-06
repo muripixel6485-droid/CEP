3ac4d1ad28ae3e49500009cfdd4395ea44505ff1
Murilo <muripixel6485@gmail.com>
2025-09-05 22:33:03 -0300

    salvo

--- script.js
+++ script.js
@@ -2,9 +2,24 @@ document.querySelector('buscar').addEventListener('click', ()=>{
 
     const cep = document.querySelector("#CEP").value.trim()
 
-    if(!CEP){
+    if(!cep){
         alert("Digite um CEP")
         return
     }
+
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(resp => resp.json())
+    .then(dados => {
+        if(dados.erro){
+            document.querySelector("#saida").textContent = 
+            "CEP não foi encontrado!"
+        }else{
+            let str = JSON.stringify(dados, null, 2)
+            document.querySelector("#saida").textContent = str
+        }
+
+    })
+
+
 
 })
