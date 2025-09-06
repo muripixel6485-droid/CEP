09d26bd9608d2dbc4696413a7d80aed8af7b97af
Murilo <muripixel6485@gmail.com>
2025-09-05 21:28:17 -0300

    script

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelector('buscar').addEventListener('click', ()=>{
+
+    const cep = document.querySelector("#CEP").value.trim()
+
+    if(!CEP){
+        alert("Digite um CEP")
+    return
+    }
+    
+})
